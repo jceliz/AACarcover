35d38b901b02b02b3b419cd922619cb4fd7e2144
rocardz <rocardz@yahoo.com>
2022-04-18 17:46:48 +0800

    RANGE ROVER

--- config2.cpp
+++ config2.cpp
@@ -82,6 +82,65 @@ class CfgVehicles
 		hiddenSelectionsMaterials[] = {"CarCover\data\BlueTarp.rvmat"};
 	};
 
+};
+
+{
+	class HouseNoDestruct;
+	class Inventory_Base;
+	class Container_Base;
+	class WorldContainer_Base;
+	class CarCoverBase;
+
+	class LANEvoque2016_White_Cover: CarCoverBase
+	{
+		scope = 2;
+		displayName = "#str_cfgvehicles_camonet0";
+		model = "CarCover\data\HMVVEEErag_Cover.p3d";
+		attachments[] =
+		{
+			"Shoulder1",
+            "Shoulder2",
+            "Shoulder3",
+            "Shoulder4",
+            "CanisterGasoline",
+            "Back",
+            "SparkPlug",
+            "CarBattery",
+            "CarRadiator",
+            "HeadlightH7",
+            "HeadlightH7",
+            "LANEvoque2016_Driver",
+            "LANEvoque2016_CoDriver",
+            "LANEvoque2016_Trunk",
+            "LANEvoque2016_Hood",
+            "CanisterGasoline",
+            "AliceBag_Green",
+            "LANEvoque2016_Wheel_1",
+            "LANEvoque2016_Wheel_1",
+            "LANEvoque2016_Wheel_1",
+            "LANEvoque2016_Wheel_1",
+            "LANEvoque2016_Wheel_2",
+            "LANEvoque2016_Wheel_2",
+            "LANEvoque2016_Wheel_2",
+            "LANEvoque2016_Wheel_2",
+            "LANEvoque2016_Wheel_3",
+            "LANEvoque2016_Wheel_3",
+            "LANEvoque2016_Wheel_3",
+            "LANEvoque2016_Wheel_3"
+
+		};
+	};
+	class LANEvoque2016_White_CoverWinter: LANEvoque2016_White_Cover
+	{
+		hiddenSelections[] = {"Carcover"};
+		hiddenSelectionsTextures[] = {"CarCover\data\CarCoverWinter.paa"};
+	};
+	class LANEvoque2016_White_CoverCivil: LANEvoque2016_White_Cover
+	{
+		hiddenSelectionsTextures[] = {"CarCover\data\BlueTarp_co.paa"};
+		hiddenSelectionsMaterials[] = {"CarCover\data\BlueTarp.rvmat"};
+	};
+
 
 
 
